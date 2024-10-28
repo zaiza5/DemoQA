f0839c04a42610586697f0d7a8772d3bb7d30179
Dell <Dell@DESKTOP-K2CT2V0.lan>
2024-10-28 08:41:04 +0200

    LoginDemo

--- src/test/java/Selenum_Training/DemoQa.java
+++ src/test/java/Selenum_Training/DemoQa.java
@@ -1,7 +1,6 @@
 package Selenum_Training;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,29 +13,30 @@ public class DemoQa {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		
 		driver.get("https://demoqa.com/automation-practice-form");
 		driver.manage().window().maximize();
 		
 		
 		WebElement usrName = driver.findElement(By.id("firstName"));
-		usrName.sendKeys("Chimane");
+		usrName.sendKeys("Name");
 		
 		WebElement usrLastName = driver.findElement(By.xpath("//*[@id=\'lastName\']"));
-		usrLastName.sendKeys("Morekhure");
+		usrLastName.sendKeys("Surname");
 		
 		WebElement emailAddress = driver.findElement(By.id("userEmail"));
-		emailAddress.sendKeys("cjmorekhure@gmail.com");
+		emailAddress.sendKeys("test@gmail.com");
 		
 		WebElement genderRadioButton = driver.findElement(By.xpath("//*[@id=\'genterWrapper\']/div[2]/div[1]/label"));
 		genderRadioButton.click();
 		
 		WebElement cellNumber = driver.findElement(By.id("userNumber"));		
-		cellNumber.sendKeys("0743720832");
+		cellNumber.sendKeys("0712345678");
 		
 		WebElement subject = driver.findElement(By.id("subjectsContainer"));
 		subject.sendKeys("This is Chimane  Testing");
+		
 		
 				
 	}
