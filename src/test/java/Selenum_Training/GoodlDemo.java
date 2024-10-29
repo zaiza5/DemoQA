5862054fd5dc55fd114a81b16d47da9d2d05a2b3
Dell <Dell@DESKTOP-K2CT2V0.lan>
2024-10-29 04:52:43 +0200

    Test

--- src/test/java/Selenum_Training/GoodlDemo.java
+++ src/test/java/Selenum_Training/GoodlDemo.java
@@ -11,7 +11,7 @@ public class GoodlDemo {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://testing.co.co.za");
+		driver.get("https://gmail.co.co.za");
 		WebElement loginButton = driver.findElement(By.id("js-ddid-btn"));
 		loginButton.click();
 		//login
