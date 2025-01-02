b9a1253b86edb88f5576d1636387fc50f938ad45
Dell <Dell@DESKTOP-K2CT2V0.lan>
2025-01-02 12:11:09 +0200

    02/01/2025 automation_Register User

--- src/test/java/Selenum_Training/AutomationExercise.java
+++ src/test/java/Selenum_Training/AutomationExercise.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 
 public class AutomationExercise {
 
@@ -22,15 +23,71 @@ public class AutomationExercise {
 		
 		
 		driver.get("https://automationexercise.com/login");
-		WebElement name = driver.findElement(By.xpath("//*//input[@name=\"name\"]"));
-		name.sendKeys("Chimane");		
+		driver.findElement(By.xpath("//*//input[@name=\"name\"]")).sendKeys("Chimane");			
+		driver.findElement(By.xpath("//input[@data-qa=\"signup-email\"]")).sendKeys("cjmorekhure@gmail.com");
+		driver.findElement(By.xpath("//button[@data-qa=\"signup-button\"]")).click();
 		
-		WebElement psswrd = driver.findElement(By.xpath("//input[@data-qa=\"signup-email\"]"));
-		psswrd.sendKeys("cjmorekhure@gmail.com");
+		//complete the sign up details
+		driver.findElement(By.id("id_gender1")).click();
+		driver.findElement(By.id("password")).sendKeys("Password1");
 		
-		WebElement signupbuttn = driver.findElement(By.xpath("//button[@data-qa=\"signup-button\"]"));
-		signupbuttn.click();
-		System.out.println("You have successfully typed your details");
+		//Day of birth
+		WebElement day = driver.findElement(By.id("days"));
+		Select dayOfBirth = new Select(day);
+		dayOfBirth.selectByIndex(18);
+		
+		//Month of birth
+		WebElement month = driver.findElement(By.id("months"));
+		Select monthOftheYear = new Select(month);
+		monthOftheYear.selectByIndex(01);
+		
+		//Year of birth
+		WebElement yearofBirth = driver.findElement(By.id("years"));
+		Select year = new Select(yearofBirth);
+		year.selectByVisibleText("1982");
+		
+		//ticking the newsletter
+		driver.findElement(By.xpath("//*[@for='newsletter']")).click();
+		
+		//Address infomation
+		driver.findElement(By.id("first_name")).sendKeys("Chimane");
+		driver.findElement(By.id("last_name")).sendKeys("Morekhure");
+		driver.findElement(By.id("address1")). sendKeys("516 Ralihlaba Street");
+		driver.findElement(By.id("address2")).sendKeys("Phalime Section");
+		
+		//selecting the country
+		WebElement countries = driver.findElement(By.id("country"));
+		Select country = new Select(countries);
+		country.selectByVisibleText("Australia");
+		
+		driver.findElement(By.id("state")).sendKeys("Germiston");
+		driver.findElement(By.id("city")).sendKeys("Katlehong");
+		driver.findElement(By.id("zipcode")).sendKeys("1432");
+		driver.findElement(By.id("mobile_number")).sendKeys("072 123 4567");
+		
+		//create button click
+		driver.findElement(By.xpath("//*[@type=\'submit\'][1]")).click();
+		System.out.println("Account infmation has been created successfully");
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		System.out.println("day of birth is "+ day.getText());
+		
+		
+		//System.out.println("You have successfully typed your details");
+		
+		
 	
 		
 		
